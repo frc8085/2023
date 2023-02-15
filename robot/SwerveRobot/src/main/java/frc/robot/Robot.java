9204a4e0b08d04f949b032cc6bfbe4e4f8c2066a
lseluga <loriann.higashi@yale.edu>
2023-02-15 13:21:58 -0500

    adding in the code from last year to make sure autonomous quits if anything is still running when auto expires

--- robot/SwerveRobot/src/main/java/frc/robot/Robot.java
+++ robot/SwerveRobot/src/main/java/frc/robot/Robot.java
@@ -5,6 +5,7 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
@@ -21,6 +22,8 @@ public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
 
   private RobotContainer m_robotContainer;
+
+  private final Timer m_timer = new Timer();
 
   /**
    * This function is run when the robot is first started up and should be used
@@ -90,6 +93,9 @@ public class Robot extends TimedRobot {
   /** This function is called periodically during autonomous. */
   @Override
   public void autonomousPeriodic() {
+    if (m_timer.get() > 14.9 && m_autonomousCommand != null) {
+      m_autonomousCommand.cancel();
+    }
   }
 
   @Override
