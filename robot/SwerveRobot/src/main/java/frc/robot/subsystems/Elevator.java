7ca4edc07709d02fbad9a6e6fb40440c1cc12a21
lseluga <loriann.higashi@yale.edu>
2023-02-17 11:41:22 -0500

    Update Elevator.java

--- robot/SwerveRobot/src/main/java/frc/robot/subsystems/Elevator.java
+++ robot/SwerveRobot/src/main/java/frc/robot/subsystems/Elevator.java
@@ -43,7 +43,7 @@ public class Elevator extends SubsystemBase {
   }
 
   public void ElevatorIsInTravelPosition() {
-    if (isElevatorTopLimitHit() && isElevatorTopLimitHit()) {
+    if (isElevatorTopLimitHit() && isArmRetractionLimitHit()) {
       TravelPosition = true;
     }
   }
