8e9d9c2004843bc836711571fe78708bcddffe05
frc-8085 <61363993+frc-8085@users.noreply.github.com>
2023-02-16 19:56:55 -0500

    updated constants with slew

--- robot/SwerveRobot/src/main/java/frc/robot/Constants.java
+++ robot/SwerveRobot/src/main/java/frc/robot/Constants.java
@@ -70,7 +70,7 @@ public final class Constants {
     // maxSpeedMetersPerSecond default = 4.8
     public static final double kMaxSpeedMetersPerSecond = 3.5;
  // reduced this from 2 * Math.PI to slow down rotation
-    public static final double kMaxAngularSpeed = 1 * Math.PI; // radians per second
+    public static final double kMaxAngularSpeed = .5 * Math.PI; // radians per second
 
     // Chassis configuration
     public static final double kTrackWidth = Units.inchesToMeters(24);
@@ -165,9 +165,9 @@ public final class Constants {
     public static int kOperatorControllerPort = 1;
     public static final double kDriveDeadband = 0.05;
     public static final double kMagnitudeDeadband = 0.05;
-    public static final double kDirectionSlewRate = 1.2; // radians per second
-    public static final double kMagnitudeSlewRate = 1.8; // percent per second (1 = 100%)
-    public static final double kRotationalSlewRate = 2.0; // percent per second (1 = 100%)
+    public static final double kDirectionSlewRate = 2.4; // radians per second
+    public static final double kMagnitudeSlewRate = 3.6; // percent per second (1 = 100%)
+    public static final double kRotationalSlewRate = 4.0; // percent per second (1 = 100%)
   }
 
   public static final class AutoConstants {
