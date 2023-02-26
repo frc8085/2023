887ec4377b6b9a5d0d358cd8c001307b48c7415b
lseluga <loriann.higashi@yale.edu>
2023-02-26 09:33:32 -0500

    two return to travel sequences depending on whether going from intake or scoring/shelf

---
+++ robot/SwerveRobot/src/main/java/frc/robot/commands/PrepareTravelAfterIntake.java
@@ -0,0 +1,29 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
+
+import static frc.robot.Constants.ExtensionConstants;
+import static frc.robot.Constants.AltitudeConstants;
+
+import frc.robot.subsystems.Altitude;
+import frc.robot.subsystems.Extension;
+
+public class PrepareTravelAfterIntake extends SequentialCommandGroup {
+    public PrepareTravelAfterIntake(
+            Extension m_extension,
+            Altitude m_altitude) {
+        addCommands(
+                new InstantCommand(() -> m_altitude.keepPosition(AltitudeConstants.kAltitudeTravelPosition)),
+                new WaitUntilCommand(() -> m_altitude.AltitudeIsInScoringPosition()),
+                new InstantCommand(
+                        () -> m_extension.keepPosition(ExtensionConstants.kExtensionPositionFullyRetracted)));
+
+    }
+}
