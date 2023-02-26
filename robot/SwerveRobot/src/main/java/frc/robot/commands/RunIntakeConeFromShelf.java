3de1a46d8f64aac6bdd7867d7518ad8d10aa4782
lseluga <loriann.higashi@yale.edu>
2023-02-26 09:34:21 -0500

    added intake from shelf option

---
+++ robot/SwerveRobot/src/main/java/frc/robot/commands/RunIntakeConeFromShelf.java
@@ -0,0 +1,24 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.subsystems.IntakeNoPID;
+import frc.robot.subsystems.Altitude;
+import frc.robot.subsystems.Extension;
+
+public class RunIntakeConeFromShelf extends SequentialCommandGroup {
+    public RunIntakeConeFromShelf(
+            Altitude m_altitude,
+            Extension m_extension,
+            IntakeNoPID m_intake) {
+        addCommands(
+                // 1. Prepare Shelf Pickup
+                new PrepareShelfPickup(m_extension, m_altitude),
+                // Run intakeCone from Shelf
+                new InstantCommand(() -> m_intake.intakeCone()));
+    }
+}
