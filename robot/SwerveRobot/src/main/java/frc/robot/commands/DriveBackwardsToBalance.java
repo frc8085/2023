a4b4255ae04a92e93242c531f540ec405808cc38
frc-8085 <61363993+frc-8085@users.noreply.github.com>
2023-03-04 16:17:01 -0500

    wait to tip and return back

--- robot/SwerveRobot/src/main/java/frc/robot/commands/DriveBackwardsToBalance.java
+++ robot/SwerveRobot/src/main/java/frc/robot/commands/DriveBackwardsToBalance.java
@@ -9,6 +9,7 @@ import frc.robot.subsystems.DriveSubsystem;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.RunCommand;
 
 public class DriveBackwardsToBalance extends CommandBase {
   @SuppressWarnings({ "PMD.UnusedPrivateField", "PMD.SingularField" })
@@ -17,6 +18,9 @@ public class DriveBackwardsToBalance extends CommandBase {
   private double m_slowSpeed = 0;
   private boolean reachedChargingStation = false;
   private boolean isBalanced = false;
+
+  private double currentPitch;
+  private boolean pitchTipped;
 
   public DriveBackwardsToBalance(DriveSubsystem drive, double speed, double slowSpeed) {
     m_drive = drive;
@@ -37,19 +41,20 @@ public class DriveBackwardsToBalance extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    double currentPitch = m_drive.getPitch();
-
-    SmartDashboard.putBoolean("REACHED STATION", reachedChargingStation);
-    SmartDashboard.putBoolean("BALACED", reachedChargingStation && isBalanced);
+    currentPitch = m_drive.getPitch();
+    pitchTipped = reachedChargingStation && currentPitch < 0;
 
     if (currentPitch >= 10) {
       reachedChargingStation = true;
     }
 
+    SmartDashboard.putBoolean("REACHED STATION", reachedChargingStation);
+    SmartDashboard.putBoolean("BALACED", reachedChargingStation && isBalanced);
+
     m_drive.drive(
         false,
-        reachedChargingStation ? m_slowSpeed : m_speed,
-        AutoConstants.kTravelBackwards,
+        reachedChargingStation ? pitchTipped ? m_slowSpeed / 2 : m_slowSpeed : m_speed,
+        pitchTipped ? AutoConstants.kTravelForwards : AutoConstants.kTravelBackwards,
         0,
         0,
         true,
@@ -59,13 +64,13 @@ public class DriveBackwardsToBalance extends CommandBase {
   // Stop driving when the command ends or is interrupted
   @Override
   public void end(boolean interrupted) {
-    m_drive.lock();
+    new RunCommand(m_drive::lock, m_drive);
   }
 
   // End the command when we reach the desired pose in meters
   @Override
   public boolean isFinished() {
-    isBalanced = m_drive.getPitch() < 7;
+    isBalanced = currentPitch > 0 && currentPitch < 3;
     return reachedChargingStation && isBalanced;
   }
 }
