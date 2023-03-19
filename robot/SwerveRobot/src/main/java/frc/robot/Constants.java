3a2f008d2d3de4d182cfe2948d099bbca7b1595f
frc-8085 <61363993+frc-8085@users.noreply.github.com>
2023-03-19 11:29:53 -0400

    Update slew

--- robot/SwerveRobot/src/main/java/frc/robot/Constants.java
+++ robot/SwerveRobot/src/main/java/frc/robot/Constants.java
@@ -33,7 +33,7 @@ public final class Constants {
 
     // Note - we had changed this to 2.4 direction, 3.6 magnitude, and 4.0
     // rotational
-    public static final double kDirectionSlewRate = 1.2; // radians per second
+    public static final double kDirectionSlewRate = 2.4; // radians per second
     public static final double kMagnitudeSlewRate = 1.8; // percent per second (1 = 100%)
     public static final double kRotationalSlewRate = 2.0; // percent per second (1 = 100%)
 
