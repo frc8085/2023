102013bd13484e67fd174569005f86bc4b4eb58a
lseluga <loriann.higashi@yale.edu>
2023-03-24 14:32:55 -0400

    using manual eject cone sequence for auto

--- robot/SwerveRobot/src/main/java/frc/robot/commands/Autos/Shared/ScoreHigh/AutoEjectCone.java
+++ robot/SwerveRobot/src/main/java/frc/robot/commands/Autos/Shared/ScoreHigh/AutoEjectCone.java
@@ -5,11 +5,13 @@
 package frc.robot.commands.Autos.Shared.ScoreHigh;
 
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.Constants.ExtensionConstants;
 import frc.robot.Constants.IntakeConstants;
 import frc.robot.commands.Extend;
+import frc.robot.commands.MoveToTravelAfterScoring;
 import frc.robot.subsystems.Altitude;
 import frc.robot.subsystems.Extension;
 import frc.robot.subsystems.Intake;
@@ -21,10 +23,16 @@ public class AutoEjectCone extends SequentialCommandGroup {
                         Extension m_extension,
                         Intake m_intake) {
                 addCommands(
+                                // Start Retracting at fixed speed until it reaches release position
                                 new InstantCommand(() -> m_extension.retractExtension())
                                                 .until(m_extension::ExtensionIsInReleasePosition),
-                                new InstantCommand(() -> m_intake.ejectCone()),
-                                new WaitCommand(IntakeConstants.kAutoEjectWaitTime),
-                                new InstantCommand(m_intake::stopIntake));
+                                new ParallelCommandGroup(
+                                                new SequentialCommandGroup(
+                                                                new InstantCommand(() -> m_intake.ejectCone()),
+                                                                new WaitCommand(IntakeConstants.kEjectWaitTime),
+                                                                new InstantCommand(m_intake::stopIntake)),
+                                                new MoveToTravelAfterScoring(m_extension, m_altitude))
+
+                );
         }
 }
