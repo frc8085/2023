2d1747649018df685e63963064a327bf8d2e22bf
lseluga <loriann.higashi@yale.edu>
2023-03-25 11:59:58 -0400

    Test Poses relative to field instead of robot

--- robot/SwerveRobot/src/main/java/frc/robot/FieldLandmarks.java
+++ robot/SwerveRobot/src/main/java/frc/robot/FieldLandmarks.java
@@ -13,6 +13,8 @@ import edu.wpi.first.math.geometry.Translation2d;
  */
 public final class FieldLandmarks {
     private static boolean PRACTICE = true;
+    public static final double BluePickupX = PRACTICE ? 5.28 : 6.5;
+    public static final double MidStationY = PRACTICE ? 3.12 : 2.7;
 
     // Intake always facing toward the grid
     public static final class GridPosition {
@@ -33,8 +35,7 @@ public final class FieldLandmarks {
 
     // Pickup positions. Intake always facing toward
     public static final class PickupPosition {
-        public static final double BluePickupX = PRACTICE ? 5.28 : 6.5;
-        public static final Pose2d Blue1 = new Pose2d(BluePickupX, 4.59, Rotation2d.fromDegrees(0));
+        public static final Pose2d Blue1 = new Pose2d(BluePickupX, 4.59, Rotation2d.fromDegrees(-10));
         public static final Pose2d Blue2 = new Pose2d(BluePickupX, 3.35, Rotation2d.fromDegrees(0));
         public static final Pose2d Blue3 = new Pose2d(BluePickupX, 2.13, Rotation2d.fromDegrees(0));
         public static final Pose2d Blue4 = new Pose2d(BluePickupX, 0.91, Rotation2d.fromDegrees(0));
@@ -43,14 +44,17 @@ public final class FieldLandmarks {
     // Endpoints for segemented trajectories
     // Intake facing grid
     public static final class SegmentEndpoints {
-        public static final Pose2d ReachCharingStation = new Pose2d(5.18, 3.12, Rotation2d.fromDegrees(-180));
-        public static final Pose2d MidChargeStation = new Pose2d(3.87, 3.12, Rotation2d.fromDegrees(-180));
+        public static final Pose2d ApproachBlue1 = new Pose2d(BluePickupX - 1, 4.59, Rotation2d.fromDegrees(-10));
+        public static final Pose2d ReachCharingStation = new Pose2d(5.18, MidStationY, Rotation2d.fromDegrees(-180));
+        public static final Pose2d MidChargeStation = new Pose2d(2.73, MidStationY, Rotation2d.fromDegrees(-180));
     }
 
     // Interior waypoints, no rotation needed
     public static final class InteriorWaypoint {
         public static final double practiceDelta = PRACTICE ? 0.6 : 0;
         public static final Translation2d HaflwayToPickup = new Translation2d(4.06 - practiceDelta, 4.24);
+        public static final Translation2d HaflwayToBlue1 = new Translation2d(BluePickupX - 0.2 - practiceDelta, 4.24);
+
         // TODO UPDATE
         public static final Translation2d HaflwayToStation = new Translation2d(5.92, 3.70);
     }
--- robot/SwerveRobot/src/main/java/frc/robot/commands/Autos/Shared/Move/AutoMoveToPickup.java
+++ robot/SwerveRobot/src/main/java/frc/robot/commands/Autos/Shared/Move/AutoMoveToPickup.java
@@ -4,6 +4,7 @@
 
 package frc.robot.commands.Autos.Shared.Move;
 
+import java.lang.reflect.Field;
 import java.util.List;
 
 import edu.wpi.first.math.geometry.Pose2d;
@@ -14,6 +15,7 @@ import edu.wpi.first.math.trajectory.TrajectoryConfig;
 import edu.wpi.first.math.trajectory.TrajectoryGenerator;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.FieldLandmarks;
 import frc.robot.commands.Autos.Shared.AutoTrajectoryCommand;
 import frc.robot.subsystems.Altitude;
 import frc.robot.subsystems.DriveSubsystem;
@@ -37,12 +39,12 @@ public class AutoMoveToPickup extends SequentialCommandGroup {
     // First trajectory. All units in meters.
     Trajectory moveToPosition = TrajectoryGenerator.generateTrajectory(
         // Start at the origin facing the +X direction
-        new Pose2d(0, 0, Rotation2d.fromDegrees(-180)),
+        FieldLandmarks.GridPosition.BlueALeft,
         // Pass through these two interior waypoints, making an 's' curve path
         // NOTE: MUST have a waypoint. CANNOT be a straight line.
-        List.of(new Translation2d(1.5, -0.4)),
+        List.of(FieldLandmarks.InteriorWaypoint.HaflwayToPickup),
         // End 3 meters straight ahead of where we started, facing forward
-        new Pose2d(3, -0.2, Rotation2d.fromDegrees(-10)),
+        FieldLandmarks.SegmentEndpoints.ApproachBlue1,
         config);
 
     m_drive.zeroHeading();
