35fe6844e38609ec70e43dfb8fdbc1706ef52860
lseluga <loriann.higashi@yale.edu>
2023-03-27 00:40:59 -0400

    force wheels to lock at the end of auto maybe

---
+++ robot/SwerveRobot/src/main/java/frc/robot/commands/Autos/Shared/LockWheelsAtEndOfAuto.java
@@ -0,0 +1,20 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.Autos.Shared;
+
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.robot.subsystems.DriveSubsystem;
+
+/** Reset the heading and odometry */
+public class LockWheelsAtEndOfAuto extends SequentialCommandGroup {
+    public LockWheelsAtEndOfAuto(
+            DriveSubsystem m_drive) {
+        addCommands(
+                new WaitCommand(14.9),
+                new InstantCommand(m_drive::lock, m_drive));
+    }
+}
