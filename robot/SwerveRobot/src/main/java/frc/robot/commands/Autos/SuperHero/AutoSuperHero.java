83cfffec81760cf233710adcaae54307b49d426c
lseluga <loriann.higashi@yale.edu>
2023-03-30 00:22:05 -0400

    testing lock wheels parallel race group

--- robot/SwerveRobot/src/main/java/frc/robot/commands/Autos/SuperHero/AutoSuperHero.java
+++ robot/SwerveRobot/src/main/java/frc/robot/commands/Autos/SuperHero/AutoSuperHero.java
@@ -6,7 +6,10 @@ package frc.robot.commands.Autos.SuperHero;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.robot.commands.Autos.Shared.LockWheelsAtEndOfAuto;
 import frc.robot.commands.Autos.Shared.Move.AutoMoveOnChargeStationFromFront;
 import frc.robot.commands.Autos.Shared.Move.AutoMoveToFrontChargeStation;
 import frc.robot.commands.Autos.Shared.Move.AutoMoveToPickup;
@@ -26,16 +29,19 @@ public class AutoSuperHero extends SequentialCommandGroup {
       Extension m_extension,
       Intake m_intake) {
     addCommands(
-        // 1. Score Cone
-        new AutoScoreHighCone(m_drive, m_altitude, m_extension, m_intake),
-        // 2. Move to pickup cargo position
-        new AutoMoveToPickup(m_drive, m_altitude, m_extension),
-        // 3. Intake down and pickup cargo
-        new AutoPickupConeClean(m_drive, m_altitude, m_extension, m_intake),
-        // 4. Intake up and move to charge station
-        new AutoMoveToFrontChargeStation(m_drive, m_altitude, m_extension, m_intake),
-        // 5. Move on Charge Station
-        new AutoMoveOnChargeStationFromFront(m_drive, m_altitude, m_extension)
+        new ParallelRaceGroup(
+            new SequentialCommandGroup(
+                // 1. Score Cone
+                new AutoScoreHighCone(m_drive, m_altitude, m_extension, m_intake),
+                // 2. Move to pickup cargo position
+                new AutoMoveToPickup(m_drive, m_altitude, m_extension),
+                // 3. Intake down and pickup cargo
+                new AutoPickupConeClean(m_drive, m_altitude, m_extension, m_intake),
+                // 4. Intake up and move to charge station
+                new AutoMoveToFrontChargeStation(m_drive, m_altitude, m_extension, m_intake),
+                // 5. Move on Charge Station
+                new AutoMoveOnChargeStationFromFront(m_drive, m_altitude, m_extension)),
+            new LockWheelsAtEndOfAuto(m_drive))
     // 6. Rotate so camera can see apriltags
 
     );
