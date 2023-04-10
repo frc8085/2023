0e4be2a5c85e0a912796309f4d8dfd6ec9f9461c
frc-8085 <61363993+frc-8085@users.noreply.github.com>
2023-04-10 00:15:21 -0400

    added drive openloop ramp rate

--- robot/SwerveRobot/src/main/java/frc/robot/Constants.java
+++ robot/SwerveRobot/src/main/java/frc/robot/Constants.java
@@ -347,6 +347,8 @@ public final class Constants {
     public static final double kTurningEncoderPositionPIDMinInput = 0; // radians
     public static final double kTurningEncoderPositionPIDMaxInput = kTurningEncoderPositionFactor; // radians
 
+    public static final double kDrivingMotorOpenLoopRampRate = 0.12;
+
     public static final double kDrivingP = 0.04;
     public static final double kDrivingI = 0;
     public static final double kDrivingD = 0;
