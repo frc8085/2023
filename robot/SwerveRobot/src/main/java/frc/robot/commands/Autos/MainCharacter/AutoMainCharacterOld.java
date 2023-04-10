f5c97587d85f53168b02f831e4c3867881328068
adrianacorona <adriana.corona@gmail.com>
2023-04-10 08:18:01 -0400

    Add balance into "old" main character sequence.

--- robot/SwerveRobot/src/main/java/frc/robot/commands/Autos/MainCharacter/AutoMainCharacterOld.java
+++ robot/SwerveRobot/src/main/java/frc/robot/commands/Autos/MainCharacter/AutoMainCharacterOld.java
@@ -7,17 +7,15 @@ package frc.robot.commands.Autos.MainCharacter;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
-import frc.robot.commands.AutoTurnToDegreeGyro;
 import frc.robot.commands.Autos.Shared.Move.AutoDriveBackwardsMeters;
 import frc.robot.commands.Autos.Shared.Move.AutoDriveForwardsMeters;
-import frc.robot.commands.Autos.Shared.Move.AutoTravelToFrontChargeStation;
 import frc.robot.commands.Autos.Shared.ScoreHigh.AutoScoreHighCone;
 import frc.robot.subsystems.Altitude;
 import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.Extension;
 import frc.robot.subsystems.Intake;
 
-/** An example command that uses an example subsystem. */
+/** Original Main Character - Does not use Swerve Spline Trajectories. */
 public class AutoMainCharacterOld extends SequentialCommandGroup {
   public AutoMainCharacterOld(
       DriveSubsystem m_drive,
@@ -29,14 +27,14 @@ public class AutoMainCharacterOld extends SequentialCommandGroup {
         new AutoScoreHighCone(m_drive, m_altitude, m_extension, m_intake),
         // 2. Move to leave community
         new AutoDriveBackwardsMeters(m_drive, 4.75, 0.4),
+        // 3. Let the charging station stabilize
         new WaitCommand(1),
+        // 4. Drive onto the charging station
         new AutoDriveForwardsMeters(m_drive, 2.50),
-        new RunCommand(m_drive::lock, m_drive)
-
-    // 5. Shoot cube
-    // new AutoBalance(m_altitude, m_extension, m_intake)
-
-    );
+        // 5. Attempt to balance until time runs out
+        new AutoMainCharacterBalance(m_drive),
+        // 6. We should never reach this command, but adding as fail-safe
+        new RunCommand(m_drive::lock, m_drive));
   }
 
 }
